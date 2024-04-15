400fc67938f7120dd594d6c67d691e9c0f440bd6
TexicoNotMexico <haruhitexico0118@gmail.com>
2024-04-15 21:24:33 +0900

    なかなか順調（？）

--- src/components/Controller.tsx
+++ src/components/Controller.tsx
@@ -1,13 +1,45 @@
-import { Button, Typography } from "@mui/material";
-import LoadingButton from "@mui/lab/LoadingButton";
-import { handleFileChange, initializeTone, isInitialized, playPause } from "../controllers/audio";
+import { IconButton, Slider, Typography } from "@mui/material";
+import {
+    handleFileChange,
+    initializeTone,
+    isInitialized,
+    loadStatus,
+    playPause,
+    playStatus,
+} from "../controllers/audio";
+import { Pause, PlayArrow, UploadFile } from "@mui/icons-material";
+import * as Tone from "tone";
+import { useEffect, useState } from "react";
+import { LoadingButton } from "@mui/lab";
+
 const Controller = () => {
+    const [playbackState, setPlaybackState] = useState<[Tone.PlaybackState, number, number]>(["stopped", 0, 0]);
+    const [loadingState, setLoadingState] = useState<[boolean, string]>([true, ""]);
+
+    useEffect(() => {
+        const sliderInterval = setInterval(() => {
+            setLoadingState(loadStatus);
+            setPlaybackState([playStatus[0], playStatus[1], playStatus[2]]);
+        }, 50);
+        return () => {
+            clearInterval(sliderInterval);
+        };
+    }, []);
+
+    const getTimestamp = (seconds: number, duration: number) => {
+        const min = Math.floor(seconds / 60);
+        const sec = Math.floor(seconds % 60);
+        const remMin = Math.ceil((duration - seconds) / 60);
+        const remSec = Math.ceil((duration - seconds) % 60);
+        return `${min}:${sec.toString().padStart(2, "0")} / ${remMin}:${remSec.toString().padStart(2, "0")}`;
+    };
+
     return (
         <>
-            <Typography variant="h5" fontWeight={"bold"}>
+            <Typography variant="h5" fontWeight={"bold"} gutterBottom>
                 操作
             </Typography>
-            <Button component="label">
+            <LoadingButton component="label" variant="outlined" startIcon={<UploadFile />} loading={!loadingState[0]}>
                 読込
                 <input
                     type="file"
@@ -18,15 +50,20 @@ const Controller = () => {
                     }}
                     hidden
                 />
-            </Button>
-            <LoadingButton
+            </LoadingButton>
+            <br />
+            <IconButton
                 onClick={async () => {
                     if (!isInitialized) await initializeTone();
                     playPause();
+                    setPlaybackState([playStatus[0], playStatus[1], playStatus[2]]);
                 }}
             >
-                再生
-            </LoadingButton>
+                {playbackState[0] === "started" ? <Pause /> : <PlayArrow />}
+            </IconButton>
+            <br />
+            <Typography variant="caption">{getTimestamp(playbackState[1], playbackState[2])}</Typography>
+            <Slider value={playbackState[1]} min={0} max={playbackState[2]} />
         </>
     );
 };
--- src/controllers/audio.ts
+++ src/controllers/audio.ts
@@ -2,18 +2,25 @@ import * as Tone from "tone";
 
 export let isInitialized: boolean = false;
 
+export let loadStatus: [boolean, string] = [true, ""];
+export let playStatus: [Tone.PlaybackState, number, number] = ["stopped", 0, 0];
+export let player: Tone.Player;
+
+export let toneAnalyser: Tone.Analyser;
+
 export const initializeTone = async () => {
     await Tone.start();
+
+    Tone.Transport.scheduleRepeat(() => {
+        playStatus[1] = Tone.Transport.seconds;
+    }, 0.05);
 
     console.info("initialized");
 
     isInitialized = true;
 };
 
-export let loadStatus: [boolean, string] = [false, ""];
-export let player: Tone.Player;
-
-export let toneAnalyser: Tone.Analyser;
+let transportStopper: number;
 
 const setPlayer = async (file: File) => {
     const reader = new FileReader();
@@ -28,14 +35,26 @@ const setPlayer = async (file: File) => {
     reader.readAsArrayBuffer(file);
 
     try {
+        if (player) player.dispose();
+        if (transportStopper) Tone.Transport.clear(playStatus[2]);
+
         const audioData = await onLoad;
         const audioBuffer = await Tone.context.decodeAudioData(audioData);
         const toneAudioBuffer = new Tone.ToneAudioBuffer(audioBuffer);
 
-        player = new Tone.Player(toneAudioBuffer).toDestination();
+        player = new Tone.Player(toneAudioBuffer);
+        playStatus[2] = player.buffer.duration;
 
         toneAnalyser = new Tone.Analyser({ type: "waveform", size: 512, channels: 2 });
-        player.connect(toneAnalyser);
+        player.sync().start(0).stop(playStatus[2]).connect(toneAnalyser).toDestination();
+
+        transportStopper = Tone.Transport.schedule(() => {
+            console.log("audio ended");
+            Tone.Transport.stop();
+            playStatus[0] = Tone.Transport.state;
+        }, playStatus[2]);
+
+        await Tone.loaded();
 
         reader.onload = null;
         reader.onerror = null;
@@ -49,6 +68,9 @@ export const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const file = e.target.files?.[0];
     if (file) {
         console.log("loading...");
+        Tone.Transport.stop();
+        playStatus[0] = Tone.Transport.state;
+        Tone.Transport.seconds = 0;
         loadStatus = [false, ""];
         setPlayer(file).then(() => {
             loadStatus = [true, file.name];
@@ -59,12 +81,13 @@ export const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
 
 export const playPause = () => {
     if (loadStatus[0]) {
-        if (player.state === "stopped") {
+        if (Tone.Transport.state !== "started") {
             console.info("start");
-            player.start("+0");
-        } else if (player.state === "started") {
+            Tone.Transport.start("+0");
+        } else {
             console.info("pause");
-            player.stop("+0");
+            Tone.Transport.pause();
         }
+        playStatus[0] = Tone.Transport.state;
     }
 };
